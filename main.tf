98b3ac0d933698503187f91e6e091020a2cdd014
jose neto <j.netoass@gmail.com>
2024-09-18 18:15:04 -0300

    adicionando mais coisas

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
 terraform {
   required_version = " > 1.3.0" #comentario
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.67.0"
+    }
+  }
 }
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner = "projeto_aula_terraform"
+      managed-by = "equipe_terraform@xxx.com"
+    }
+  }
+}
+# a) init
+# b) validate
+# c) plan
+# d) apply
+# e) destroy
+# f) fmt
